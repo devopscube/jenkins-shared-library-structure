6d8c34ae20a5baf2fb82e00aa374d2459ad3594d
DevopsCube <admin@comtechies.com>
2019-07-07 10:26:15 +0530

    Update and rename git.groovy to gitCheckout.groovy

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,9 @@
+
+def call(Map stageParams) {
+ 
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
